85eb7e9dcc7ba303502f065767d2b5eba7efd54b
Valentin Lup (ext.) <Valentin.Lup@partner.bmw.de>
2024-07-15 21:00:27 +0300

    wip

--- tests/brokenImageLinksCheck.spec.js
+++ tests/brokenImageLinksCheck.spec.js
@@ -3,7 +3,7 @@ import {expect, test} from "@playwright/test";
 
 test("Check that the car image links are not broken", async ({page}) => {
     //increase test timeout because of a longer test execution
-    test.setTimeout(60000);
+    test.setTimeout(90000);
 
     //navigate to all models
     await page.goto("https://www.bmw.de/de/neufahrzeuge.html", {waitUntil: "networkidle"});
@@ -14,22 +14,43 @@ test("Check that the car image links are not broken", async ({page}) => {
     //count them
     console.log(await images.count());
 
-    //scroll to the bottom of the page to load all of them
-    await page.evaluate(() => window.scrollBy(0, document.body.scrollHeight));
+    /////////////
+    //scrolls remaining before the script exits
+    var scrollsRemaining = 30;
+    //while we have scrolls remaining
+    while (scrollsRemaining > 0) {
+        //scroll down by 10,000 pixels
+        await page.evaluate(()=> window.scrollBy(0, 10000));
+        //use a hardcoded wait time of one second for content to load
+        await page.waitForLoadState("networkidle", {
+            timeout: 10000
+        });
+        //decrement the scrolls remaining
+        scrollsRemaining--;
+    }
+    /////////////
 
-    await page.waitForTimeout(1000);
+
+    //scroll to the bottom of the page to load all of them
+    // await page.evaluate(() => window.scrollBy(0, document.body.scrollHeight));
+
+    // await page.waitForTimeout(1000);
 
     //get the src value of each image element
     const allImages = await images.all();
     for await (const img of allImages) {
         const imgSrc = await img.getAttribute("src");
         //check that it's not empty
-        expect.soft(imgSrc?.length, "Failed to load: " + imgSrc).toBeGreaterThan(1);
+        expect(imgSrc?.length).toBeGreaterThan(1);
 
         if (imgSrc?.length > 1) {
             //check that the status code of the image is 200 (no broken image)
             const response = await page.request.get(imgSrc);
-            await expect.soft(response.status(), "Failed to load: " + imgSrc).toBe(200);
+            console.log(response.status());
+
+            if (!(response.status() === 200)) {
+                console.log("Failed to load: " + imgSrc);
+            }
         }
     }
 });
