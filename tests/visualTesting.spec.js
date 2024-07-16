8f6471d6d25f76053546ffcb7d6d402b06f97a25
Valentin Lup (ext.) <Valentin.Lup@partner.bmw.de>
2024-07-16 17:40:14 +0300

    wip

---
+++ tests/visualTesting.spec.js
@@ -0,0 +1,31 @@
+import {expect, test} from "@playwright/test";
+
+test("Basic test - checks the homepage first view", async({page}) => {
+   await page.goto("https://www.bmw.de", {waitUntil: "networkidle"});
+
+   //accept cookies
+   await page.getByRole('button', {name: 'Alle akzeptieren'}).click();
+
+   //check homepage first view
+   await expect(page).toHaveScreenshot("first view.png");
+});
+
+test("Visual test - checks the homepage full page", async({page}) => {
+   await page.goto("https://www.bmw.de", {waitUntil: "networkidle"});
+
+   //accept cookies
+   await page.getByRole('button', {name: 'Alle akzeptieren'}).click();
+
+   //check homepage first view
+   await expect(page).toHaveScreenshot("homepage full page.png",{fullPage: true});
+});
+
+test.only("Visual test - check Made to Express section", async({page}) => {
+   await page.goto("https://www.bmw.de", {waitUntil: "networkidle"});
+
+   //accept cookies
+   // await page.getByRole('button', {name: 'Alle akzeptieren'}).click();
+
+   //check homepage first view
+   await expect(page.locator("[class*='container responsivegrid style-common--cmp-spacing-top-16']")).toHaveScreenshot("made to express section.png");
+});
